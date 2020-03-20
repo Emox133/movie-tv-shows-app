93f2d4e27e4363106ef2e084a45a7f4c800842d2
Emox133 <emir.betaacc0unt@gmail.com>
2020-03-20 22:45:09 +0100

    Updates

---
+++ precache-manifest.9b7a01dde0ddf7d2b9823589c3408fb7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d42e94d2e0b4d879f59ad7765b24e3a8",
+    "url": "/tv-shows/index.html"
+  },
+  {
+    "revision": "ef9ea28ba59b3e055f45",
+    "url": "/tv-shows/static/css/main.2641aec7.chunk.css"
+  },
+  {
+    "revision": "385116666c284afbe72b",
+    "url": "/tv-shows/static/js/2.13b2a413.chunk.js"
+  },
+  {
+    "revision": "e2a16c305685cdb648390c37ab05bb20",
+    "url": "/tv-shows/static/js/2.13b2a413.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef9ea28ba59b3e055f45",
+    "url": "/tv-shows/static/js/main.c1701b2a.chunk.js"
+  },
+  {
+    "revision": "665319edaf0887a884a3",
+    "url": "/tv-shows/static/js/runtime-main.8783026e.js"
+  },
+  {
+    "revision": "9151611009593aff302240c50cbbf808",
+    "url": "/tv-shows/static/media/area-graph.91516110.svg"
+  },
+  {
+    "revision": "f1592feb411b9e0d636f3329786b57d2",
+    "url": "/tv-shows/static/media/calendar.f1592feb.svg"
+  },
+  {
+    "revision": "d26a9e38edf96defe85ef60ce9d0ab0a",
+    "url": "/tv-shows/static/media/heart-outlined.d26a9e38.svg"
+  }
+]);
